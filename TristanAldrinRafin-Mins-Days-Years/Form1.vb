5e8e0206c415a3558649fd27098b67efbe33c7f4
Racyn Ibaya <racynibaya@outlook.com>
2022-11-07 07:55:53 -0500

    initial commit

---
+++ TristanAldrinRafin-Mins-Days-Years/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Text = (TextBox1.Text / (60 * 24))
+        TextBox3.Text = Math.Round(TextBox1.Text / 525949.2)
+    End Sub
+End Class
